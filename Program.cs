364b18f6d00faba33fea067c202fb77844b2a9ab
Massil <160241880+Massil-br@users.noreply.github.com>
2024-12-20 17:23:01 +0100

    folder management and file management , separation of code

--- Program.cs
+++ Program.cs
@@ -1,27 +1,12 @@
 ﻿using System;
-using System.Numerics;
-using Raylib_cs;
+using Tetris_QMJ.src.Core;
 namespace Tetris_QMJ
 {
     class Program
     {
         static void Main(string[] args)
-        {   
-            int width = 1000;
-            int heigth = 1000;
-            Raylib.SetConfigFlags(ConfigFlags.ResizableWindow);
-            Raylib.InitWindow(width ,heigth,"Tetris");
-            
-            Raylib.SetTargetFPS(60);
-            Font TitleFont = Raylib.LoadFont("assets/font/Team 401.ttf");
-            while(!Raylib.WindowShouldClose()){
-                Raylib.BeginDrawing();
-                Raylib.ClearBackground(Color.White);
-                
-                Raylib.DrawTextEx(TitleFont, "TETRIS", new Vector2((Raylib.GetRenderWidth()/2)-35,50), 40, 2, Color.Black);
-                Raylib.EndDrawing();
-            }
-            Raylib.CloseWindow();
+        {
+            src.Core.Game.InitWindow();
         }
     }
 }
