cd4df904b56e28a229a137f68942059d2b962f1c
Massil <160241880+Massil-br@users.noreply.github.com>
2024-12-20 16:57:32 +0100

    test Raylib

--- Program.cs
+++ Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
-
+using System.Numerics;
+using Raylib_cs;
 namespace Tetris_QMJ
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {   
+            int width = 1000;
+            int heigth = 1000;
+            Raylib.SetConfigFlags(ConfigFlags.ResizableWindow);
+            Raylib.InitWindow(width ,heigth,"Tetris");
+            
+            Raylib.SetTargetFPS(60);
+            Font TitleFont = Raylib.LoadFont("assets/font/Team 401.ttf");
+            while(!Raylib.WindowShouldClose()){
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.White);
+                
+                Raylib.DrawTextEx(TitleFont, "TETRIS", new Vector2((Raylib.GetRenderWidth()/2)-35,50), 40, 2, Color.Black);
+                Raylib.EndDrawing();
+            }
+            Raylib.CloseWindow();
         }
     }
 }
